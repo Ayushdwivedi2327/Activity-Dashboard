f2d4034bb2ab5c3481b6bda6cc6066f1a0e470d7
Ayush Dwivedi <ayush.dwivedi2327@gmail.com>
2026-03-23 00:32:17 +0530

    TODO-LIST-CONTINUE

--- script.js
+++ script.js
@@ -18,8 +18,40 @@ allfullelemsBack.forEach(function (back) {
 
 // openFeatures();
 let form = document.querySelector(".addTask form");
-let Taskinput = document.querySelector(".addTask form input");
+let Taskinput = document.querySelector(".addTask form #task-input");
 let Taskdetailsinput = document.querySelector(".addTask form textarea");
+let taskCheckbox = document.querySelector(".addTask form #check");
+
+let currentTask = [
+  {
+    task: "mandir jaao",
+    details: "Hanuman ji k",
+    imp: true,
+  },
+  {
+    task: "mandir jaldi jaao",
+    details: "krishna ji k",
+    imp: true,
+  },
+  {
+    task: "mandir na jaao",
+    details: "modi ji k",
+    imp: false,
+  },
+];
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+  // console.log(Taskinput.value, Taskdetailsinput.value);
+  // console.log(taskCheckbox.checked);
 });
+var allTask = document.querySelector(".allTask"); 
+// console.log(allTask)
+var sum = "";
+currentTask.forEach(function (elem) {
+  sum += ` <div class="task">
+              <h5>${elem.task}</h5>
+              <button>Mark as Completed</button>
+            </div>`;
+});
+allTask.innerHTML = sum;
